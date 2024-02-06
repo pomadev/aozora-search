86671dbb1d76532fe803854b439f37bc708520fd
pomadev <y.pomadev@gmail.com>
2024-02-07 00:03:47 +0900

    feat: 芥川龍之介の作品タイトルと作品ZIPURLを取得する

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -13,8 +17,41 @@ type Entry struct {
 	Author   string
 	TitleID  string
 	Title    string
-	InfoURL  string
+	SiteURL  string
 	ZipURL   string
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	log.Println("query", siteURL)
+	res, err := http.Get(siteURL)
+	if err != nil {
+		return "", ""
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", ""
+	}
+	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").Text()
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+	if zipURL == "" {
+		return author, ""
+	}
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, u.String()
 }
 
 func findEntries(siteURL string) ([]Entry, error) {
@@ -27,10 +64,28 @@ func findEntries(siteURL string) ([]Entry, error) {
 	if err != nil {
 		return nil, err
 	}
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	entries := []Entry{}
 	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
-		println(elem.Text(), elem.AttrOr("href", ""))
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		title := elem.Text()
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
 	})
-	return nil, nil
+	return entries, nil
 }
 
 func main() {
@@ -43,5 +98,4 @@ func main() {
 	for _, entry := range entries {
 		fmt.Println(entry.Title, entry.ZipURL)
 	}
-
 }
