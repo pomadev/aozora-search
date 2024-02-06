eefdb4c86d7ec659e4992f85ac95090ab11ec6c8
pomadev <y.pomadev@gmail.com>
2024-02-06 23:36:19 +0900

    feat: 芥川龍之介の作品一覧URLを取得する

---
+++ cmd/aozora-collector/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	InfoURL  string
+	ZipURL   string
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		println(elem.Text(), elem.AttrOr("href", ""))
+	})
+	return nil, nil
+}
+
+func main() {
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+	entries, err := findEntries(listURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, entry := range entries {
+		fmt.Println(entry.Title, entry.ZipURL)
+	}
+
+}
